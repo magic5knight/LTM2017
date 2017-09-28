55f664f9ddf98e1f612e3314018e903f335e3714
Tuan Tran <tuantranphuong@gmail.com>
2017-09-28 15:12:14 +0700

    Connectionless Socket

---
+++ UDPSocket-Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+namespace UDPSocket_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Khoi tao client!");
+            //b1 xac dinh ip server
+            IPEndPoint ipServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+            //b2 tao socket
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //b3 gui nhan data
+            Console.WriteLine("Client gui data:");
+            EndPoint epServer = (EndPoint)ipServer;
+            try
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Hello server!");    
+                client.SendTo(send, epServer);
+                byte[] rec = new byte[25];
+                client.ReceiveFrom(rec, ref epServer);
+                Console.WriteLine("Client nhan data: {0}", Encoding.ASCII.GetString(rec));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Khong the ket noi server!");
+                Console.Write(ex);
+            }
+           
+            //b4 dong client socket
+            client.Close();
+        }
+    }
+}
---
+++ UDPSocket-Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPSocket_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Khoi tao server!");
+            //b1 tao socket udp
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, 12345); 
+            //b2 ket noi socket voi card mang
+            server.Bind(ip);
+            //b3 gui nhan data
+            //server phai nhan data truoc
+            byte[] rec = new byte[25];
+            EndPoint client = (EndPoint)ip;
+            server.ReceiveFrom(rec, ref client);
+            Console.WriteLine("Server nhan data: {0}", Encoding.ASCII.GetString(rec));
+            Console.WriteLine("Server gui data!");
+            byte[] send = Encoding.ASCII.GetBytes("Hello client!");
+            server.SendTo(send, client);
+            //b4 dong server socket
+            server.Close();
+        }
+    }
+}
